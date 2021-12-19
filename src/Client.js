badfd55242ac784ff29109f014329f8a0668d694
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-12-19 13:57:18 +0700

    Create Client.js

---
+++ src/Client.js
@@ -0,0 +1,56 @@
+import clientApi from './structure/Client/api'
+import ClientUsers from './structure/Client/manager/users'
+import ClientGuilds from './structure/Client/manager/guilds'
+import ClientChannels from './structure/Client/manager/channels'
+import BaseInteraction from './structure/Interaction/baseInteraction'
+import Fastify from 'fastify'
+
+export class Client {
+  constructor(data = {}) {
+    clientApi(this)
+    
+    Object.defineProperties(this, {
+      endpoint: {
+        value: data.endpoint 
+      },
+      application: {
+        value: {
+          id: data.applicationId,
+          route: `https://discord.com/api/v9/applications/${data.applicationId}/`,
+          publicKey: data.publicKey
+        }
+      }
+    })
+    
+    // cache
+    this.users = new ClientUsers(this)
+    this.guilds = new ClientGuilds(this)
+    this.channels = new ClientChannels(this)
+  }
+  
+  
+  async addCommand(data, guildId = null) {   
+    if(guildId === null) {
+      return (await this.api.post(this.application.route + 'commands', data))
+    } 
+    else {
+      return (await this.api.post(`${this.application.route}guilds/${guildId}/commands`, data))
+    }
+  }
+  
+  
+  createServer(port) {
+    const server = Fastify()
+    
+    server.post(this.endpoint, (req, res) => {
+      const interaction = new BaseInteraction(req, res)
+      if(interaction.validate() === true) {
+        this.emit('interaction', interaction)
+      }
+    })
+    
+    server.listen(port)
+  }
+  
+  
+}
