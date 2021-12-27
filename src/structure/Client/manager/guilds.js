b1bdcacce5e7f6b43efd67269e73e8b6e7ae4e4a
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-12-27 12:31:39 +0700

    Create guilds.js

---
+++ src/structure/Client/manager/guilds.js
@@ -0,0 +1,11 @@
+import Cache from '../../../Cache';
+import Guild from '../../Guild/guild';
+
+export class ClientGuilds extends Cache {
+  constructor(client, options) {
+    super(client, {
+      path: 'https://discord.com/api/v9/guilds/',
+      fetchReturnType: Guild,
+      expiredTime: options.expiredTime
+    })
+}
